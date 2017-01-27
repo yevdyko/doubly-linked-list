a3562e50ea81575723953b52f1c9d38ea7e096a1
Yev Dyko <evgeny.dyko@gmail.com>
2017-01-28 02:20:31 +0300

    doubly-linked-list/feature: update append method
    Add new data to the end of list.

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,10 +9,17 @@ class LinkedList {
 
     append(data) {
         let node = new Node(data);
+        
         if (this.length === 0) {
             this._head = node;
+            this._tail = node;      
+        } else {
+            this._tail.next = node;
+            node.prev = this._tail;
             this._tail = node;
         }
+        
+        this.length++;
     }
 
     head() {}
